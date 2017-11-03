e77b06e77e37dba9855915d603dcf4bfa87d561f
qingfawang <qingfawang@gmail.com>
2017-11-03 15:52:10 +0800

    new headers

--- miniBond/views.py
+++ miniBond/views.py
@@ -73,8 +73,13 @@ def strongStaticView(contextDict, templateName, staticFileName):
 def refreshCache(request):
     # allpfs = Platform.objects.filter(isValid=True)
     allpfs = Platform.objects.all()
+    # pfs = [p for p in allpfs if
+    # p.promotioninfo_set.filter(isValid=True).count() > 0 or (hasattr(p,
+    # 'linktowx') and p.linktowx.isValid)]
+
     pfs = [p for p in allpfs if
-           p.promotioninfo_set.filter(isValid=True).count() > 0 or (hasattr(p, 'linktowx') and p.linktowx.isValid)]
+           (hasattr(p, 'linktowx') and p.linktowx.isValid)]
+
     contextDict = {'pfs': pfs}
     templateName = "index.html"
     strongStaticView(contextDict, templateName, templateName)
