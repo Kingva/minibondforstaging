cd4ab9325afb508a02e54f082493ae7ffb2897bb
qingfawang@gmail.com <qingfawang@gmail.com>
2018-06-22 16:32:24 +0800

    a

--- crawler/discuz.py
+++ crawler/discuz.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     faildlist = []
     maxtrytosubmit = 20
 
-    for aticle in lsittopublish[0:36]:
+    for aticle in lsittopublish[0:28]:
         print(aticle.username)
         robot = DiscuzRobot(siteurl,
                             aticle.username, password)
