cd58546628995bd463c0c5aa42cbc2cf8a4dc3f8
Kingva <qingfawang@gmail.com>
2018-07-02 08:12:39 +0800

    a

--- crawler/discuz.py
+++ crawler/discuz.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     faildlist = []
     maxtrytosubmit = 20
 
-    for aticle in lsittopublish[0:32]:
+    for aticle in lsittopublish[0:23]:
         print(aticle.username)
         robot = DiscuzRobot(siteurl,
                             aticle.username, password)
