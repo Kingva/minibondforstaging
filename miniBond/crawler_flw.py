ca38be487f9d146110e9c72d458dabb48a705063
qingfawang@gmail.com <qingfawang@gmail.com>
2018-08-17 14:43:20 +0800

    xcx

--- miniBond/crawler_flw.py
+++ miniBond/crawler_flw.py
@@ -25,6 +25,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        192.168.20.75
+        # :27017
     }
 }
 # 给Django配置相关信息
---
+++ xiaoChx/pages/search/search.js
@@ -0,0 +1,74 @@
+// search.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    products: [
+      {
+        productName: '宅易通', bank: '平安银行', ammountText: "1500", rateText: "5.655-7.35", yearText: "10年"},
+      {
+        productName: '房抵贷', bank: '浙商银行', ammountText: "100-1000", rateText: "6.96-8", yearText: "10年"},
+      {
+        productName: '个人商务快捷贷', bank: '邮储银行', ammountText: "1000", rateText: "5.22-6.37", yearText: "10年"
+}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
